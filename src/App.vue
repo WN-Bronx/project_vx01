eafdcdb2cab2e96ff54a5b3a2155ab628c0db406
WN-Bronx <dj.pretto@hotmail.com>
2020-09-08 20:51:07 -0300

    Commitando

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="principal">
+    <h1>SORTEIO do BRONX</h1>
+    <div>
+      <div class="name">
+          <input type="text" name="nome" placeholder="Informe seu nome" required="required" autofocus>
+      </div>
+      <div class="email">
+          <input type="email" name="email" placeholder="Informe seu e-mail" required="required">
+      </div>
+      <button @click="addinfo()">Add Dado</button>
+    </div>
+    <button @click="sortear()">Click para Sortear</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    nome: 'app'
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+#principal{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #2c3e50;
+  border: 5px solid silver;
+  color: blue;
+  margin: 100px;
 }
+
 </style>

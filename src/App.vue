ca98ada381ce05becb9b3febda9b4da18800d6ad
WN-Bronx <dj.pretto@hotmail.com>
2020-09-11 21:35:28 -0300

    Commit Up

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,46 @@
 <template>
-  <div id="principal">
-    <h1>SORTEIO do BRONX</h1>
-    <div>
-      <div class="name">
-          <input type="text" name="nome" placeholder="Informe seu nome" required="required" autofocus>
-      </div>
-      <div class="email">
-          <input type="email" name="email" placeholder="Informe seu e-mail" required="required">
-      </div>
-      <button @click="addinfo()">Add Dado</button>
-    </div>
-    <button @click="sortear()">Click para Sortear</button>
-  </div>
+  <div class id="app">
+    <div class="container grid-xs py-2">
+      <img class="img-responsive img-logo" src="@/assets/photo.png" alt="Logo">
+        <div>  
+          <form>
+            <!-- Vou Entrar com as minhas informações -->
+            <div class="input-group entrada-info">
+              <input type="text" v-model="todo.description" class="form-input" id="name" placeholder="Name">  
+              <input type="email" v-model="todo.description" class="form-input" id="email" placeholder="E-mail">            
+              <button class="btn input-group-btn">Add Informação</button>
+              {{ todo }}
+            </div>
+          </form>
+        </div>  
+    </div>  
+  </div>  
 </template>
 
 <script>
   export default {
-    nome: 'app'
+    nome: 'app',
+    // Data será o estado do meu componente, vai retornar meus dados    
+    data(){
+      return {todos:[], todo:{} };
+    },
+    // Methods informações dentro dos componentes    
+    methods:{
+      addTodo() {}
+    }
   }
 </script>
 
-<style>
+<!-- Scoped so serve para a clase atual-->
+<style scoped>
+.img-logo{
+  max-width: 100px;
+  margin: 0 auto;
+}
 
-#principal{
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  background-color: #2c3e50;
-  border: 5px solid silver;
-  color: blue;
-  margin: 100px;
+.entrada-info{
+  max-width: 350px;
+  margin: 0 auto;
 }
 
 </style>
